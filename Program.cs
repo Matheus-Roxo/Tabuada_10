3f97d10275d9df81b41c5331a7a38c16ce8d1c8a
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-12-23 01:17:08 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tabuada_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual Tabuada você quer saber?");
+            int tabuada = int.Parse(Console.ReadLine());
+
+            int contador = 0;
+
+            while(contador <= 10){
+
+                Console.WriteLine(tabuada * contador);
+                contador = contador + 1;
+                
+            }
+        }
+    }
+}
